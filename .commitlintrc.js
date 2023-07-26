20c2887c5c793191fe23db56c52235596eabf357
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-07-26 11:15:05 -0400

    ci: add commitlint, husky, and package-lock

---
+++ .commitlintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+    "scope-case": [
+      2,
+      "always",
+      "lower-case"
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build', // Changes that affect how code is built (e.g. npm, tsconfig, Dockerfile, etc.)
+        'ci', // Changes to CI config files and scripts (e.g. GH Workflows, SonarCloud, ESLint, etc.)
+        'deps', // Changes to project dependencies
+        'docs', // Documentation changes
+        'feat', // A new feature (corresponds to a minor version). Use "feat!" for a breaking change
+        'fix', // A fix for an issue (corresponds to a patch version)
+        'refactor', // A rework of existing code that does not change its behaviors or externals
+        'revert', // A revert of a previous commit to cancel out its changes
+        'test', // Changes/additions to tests
+        'wip' // A work-in-progress commit, mostly for local use. Not intended for pushes to main
+      ]
+    ]
+  }
+}

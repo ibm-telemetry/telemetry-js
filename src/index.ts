d6e39bc8162286b3dd068a1b7d21cb6a39988ca9
Francine Lucca <40550942+francinelucca@users.noreply.github.com>
2023-07-27 11:18:23 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-export const abc = 123
+export const abc = 1234

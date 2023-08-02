80254e220b022a037a7c1d1d383bd3f8b8b32183
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-08-02 11:04:20 -0400

    feat(core): add basic otel initialization logic
    Co-authored-by: Francine Lucca <francinelucca@users.noreply.github.com>

---
+++ src/main/core/resource-attributes.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright IBM Corp. 2023, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//
+// General attributes
+//
+export const DATE = 'date'
+export const PROJECT_ID = 'project.id'
+
+//
+// Attributes relating to details about the currently-running emitter
+//
+export const EMITTER_NAME = 'transistor.emitter.name'
+export const EMITTER_VERSION = 'transistor.emitter.version'
+
+//
+// Attributes relating to details about the analyzed repository
+//
+export const ANALYZED_RAW = 'analyzed.raw'
+export const ANALYZED_HOST = 'analyzed.host'
+export const ANALYZED_OWNER = 'analyzed.owner'
+export const ANALYZED_REPOSITORY = 'analyzed.repository'

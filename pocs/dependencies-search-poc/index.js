2faa8c1423ff298d2bb15fe8f59a5b37a760c08e
Francine Lucca <francinelucca@hotmail.com>
2023-09-01 12:59:27 -0400

    fix: make findPkgInProject async and cleanup exec function

--- pocs/dependencies-search-poc/index.js
+++ pocs/dependencies-search-poc/index.js
@@ -4,13 +4,15 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import { existsSync } from 'node:fs'
+import { promises as fs } from 'node:fs'
 import path from 'node:path'
 
 import lodash from 'lodash'
 import objectScan from 'object-scan'
 
 import { exec } from './exec.js'
+
+const fileExists = async (path) => Boolean(await fs.stat(path).catch(() => false))
 
 const parsePkgJSONContents = (json, pkgName, pkgVersion) => {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- blah
@@ -35,32 +37,33 @@ const parsePkgJSONContents = (json, pkgName, pkgVersion) => {
   })
 }
 
-const findPkgInProject = (pkgName, pkgVersion) => {
+const findPkgInProject = async (pkgName, pkgVersion) => {
   const installingPackages = []
   // Assume we are in a sub-folder of a node_modules folder. Step out of it
+  let prevCurrentPath = null
   let currentPath = path.join(process.cwd(), '..', '..')
 
   // what happens if we run out of paths? would that even happen?
-  while (installingPackages.length === 0 && currentPath !== '' && currentPath !== '../') {
+  while (installingPackages.length === 0 && currentPath !== prevCurrentPath) {
     const pkgJsonPath = path.join(currentPath, '/package.json')
-    if (existsSync(pkgJsonPath)) {
+    if (await fileExists(pkgJsonPath)) {
       const pkgJSONContents = JSON.parse(exec('npm ls --all --json', { cwd: currentPath }))
       installingPackages.push(...parsePkgJSONContents(pkgJSONContents, pkgName, pkgVersion))
     }
+    prevCurrentPath = currentPath
     currentPath = path.join(currentPath, '..')
   }
   return installingPackages
 }
 
 const getCurrentPkgDetails = () => {
-  const name = exec('npm pkg get name').slice(1, -1)
-  const version = exec('npm pkg get version').slice(1, -1)
+  const { name, version } = JSON.parse(exec('npm pkg get name version'))
 
   return [name, version]
 }
 
 // me running everything we need to get the relevant data
 const [name, version] = getCurrentPkgDetails()
-const data = { name, version, installingPackages: findPkgInProject(name, version) }
+const data = { name, version, installingPackages: await findPkgInProject(name, version) }
 // eslint-disable-next-line vitest/require-hook -- blah
 console.log(JSON.stringify(data, null, 2))

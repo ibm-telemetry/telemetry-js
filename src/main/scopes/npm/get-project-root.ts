51472111401a4c2956c49bf9ab48e52b02d69c91
Francine Lucca <francinelucca@hotmail.com>
2023-09-07 14:18:38 -0400

    feat: add getProjectRoot function

--- src/main/scopes/npm/get-project-root.ts
+++ src/main/scopes/npm/get-project-root.ts
@@ -7,12 +7,26 @@
 // TODO: should this live in some other place since it might be useful to have for other scopes?
 // until we have a second scope, it might not be worth trying to centralize it
 
+import path from 'path'
+
+import { exec } from '../../core/exec.js'
+
 /**
  * Finds and returns the root-most directory of the analyzed project's source tree.
  *
- * @returns The path of the analyzed project's root directory.
+ * @returns The path of the analyzed project's root directory or null.
  */
-export function getProjectRoot(): string {
-  // TODO: Implement Lucca's algorithm! See td-06 for details
-  return 'TODO'
+export function getProjectRoot(): string | null {
+  let prevRoot = null
+  let cwd = process.cwd()
+
+  while (true) {
+    try {
+      exec('npm pkg get name', { cwd })
+      prevRoot = cwd
+      cwd = path.join(cwd, '..')
+    } catch (_e) {
+      return prevRoot
+    }
+  }
 }

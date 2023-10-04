07fbe4a6a4f225906231cfc6bd958b2679f2ab9c
Francine Lucca <francinelucca@hotmail.com>
2023-10-04 17:00:48 -0400

    wip: anonimization refactor explorations

--- src/main/core/anonymize.ts
+++ src/main/core/anonymize.ts
@@ -16,6 +16,95 @@ interface AnonymizeConfig<T extends Attributes> {
   substitute?: Array<keyof T>
 }
 
+// 1. substitute names (and their values) for attribute names that aren't in allowedAttributeNames
+// 2. substitute values that aren't in allowedAttributeValues
+
+const roughFormat = {
+  name: 'element.count',
+  unit: '',
+  sum: {
+    dataPoints: [
+      {
+        attributes: [
+          {
+            //
+            // Full-text version of the analyzed element.
+            //
+            key: 'raw',
+            value: { stringValue: "<ExampleElement exampleAttribute='exampleString'>" }
+          },
+          {
+            //
+            // Name of the JSXElement.
+            //
+            key: 'name',
+            value: { stringValue: 'ExampleElement' }
+          },
+          {
+            //
+            // Sorted list of collected attribute names on the element.
+            //
+            key: 'attributeNames',
+            value: ['exampleAttribute']
+          },
+          {
+            //
+            // List of collected attribute values on the element. The indexes of these values
+            // correspond to the indexes of the attributeNames. E.g. `attributeValues[0]` is the
+            // value for `attributeNames[0]`.
+            //
+            key: 'attributeValues',
+            value: ['exampleString']
+          }
+        ],
+        asInt: '123'
+      }
+    ]
+  }
+}
+
+const allowedAttributeNames = ['goodName1', 'goodName2']
+const allowedAttributeValues = ['good value 1']
+const exampleObj = {
+  raw: '<Button goodName1="good value 1" goodName2="secret value" secret1="good value 1" secret2="secret value" />',
+  attributes: {
+    goodName1: 'good value 1', // no change
+    goodName2: 'secret value', // sub value
+    secret1: 'good value 1', // sub name and value
+    secret2: 'secret value' // sub name and value
+  }
+}
+
+const result = {
+  raw: 'abc123abc123abc123abc123',
+  attributes: {
+    goodName1: 'good value 1',
+    goodName2: '[redacted1]',
+    '[redacted2]': '[redacted3]',
+    '[redacted4]': '[redacted1]'
+  }
+}
+
+const toMakeTheResult = anonymize(exampleObj, {hash: ['raw']})
+toMakeTheResult.attributes = anonymize(exampleObj.attributes, {substitute: []}) // doesn't work because need the config stuff
+
+const toMakeTheResultBetter = hash(exampleObj, ['raw'])
+const toMakeTheResultBetter.attributes = substitute(exampleObj.attributes, allowedAttributeNames, allowedAttributeValues) // <---
+
+const superSecretStuff = {}
+
+function substitute() {
+  if (theVal in superSecretStuff) {
+    return superSecretStuff[theVal]
+  } else {
+    const id = makeNewRedactedIdentifier()
+    superSecretStuff[theVal] = id
+  }
+}
+
+
+
+
 /**
  * Anonymizes incoming raw data. The keys to be anonymized are specified in the config object
  * under either the `hash` key or the `substitute` key.

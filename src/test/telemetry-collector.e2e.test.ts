439d2e70af9e3003f6c73d53e308291624bc3c44
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-10-23 13:04:24 -0400

    test(e2e): make e2e telemetry collector test more meaningful

--- src/test/telemetry-collector.e2e.test.ts
+++ src/test/telemetry-collector.e2e.test.ts
@@ -4,6 +4,7 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import path from 'path'
 import { afterAll, describe, expect, it } from 'vitest'
 
 import { createLogFilePath } from '../main/core/log/create-log-file-path.js'
@@ -11,6 +12,7 @@ import { Logger } from '../main/core/log/logger.js'
 import { UnknownScopeError } from '../main/exceptions/unknown-scope-error.js'
 import { TelemetryCollector } from '../main/telemetry-collector.js'
 import { type Schema } from '../schemas/Schema.js'
+import { Fixture } from './__utils/fixture.js'
 
 const logger = new Logger(await createLogFilePath(new Date().toISOString()))
 
@@ -22,8 +24,12 @@ describe('telemetryCollector', () => {
   describe('runScopes', () => {
     it('does not throw when existing scopes are specified in the config', async () => {
       const telemetryCollector = new TelemetryCollector('', '', logger)
+      const root = new Fixture(path.join('projects', 'basic-project'))
+      const cwd = new Fixture(
+        path.join('projects', 'basic-project', 'node_modules', 'instrumented')
+      )
 
-      const promises = telemetryCollector.runScopes('', '', {
+      const promises = telemetryCollector.runScopes(cwd.path, root.path, {
         projectId: 'asdf',
         version: 1,
         collect: { npm: { dependencies: null } }

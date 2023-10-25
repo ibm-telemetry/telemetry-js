18e3abbb60af1acee1f6059217b3d0de477c06a1
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-10-25 15:32:15 -0400

    fix: ibmtelemetry bin hashbang

--- esbuild.js
+++ esbuild.js
@@ -9,7 +9,14 @@ import path from 'path'
 
 await build({
   banner: {
-    js: `const require = (await import("node:module")).createRequire(import.meta.url);
+    js: `#!/usr/bin/env node
+/*
+ * Copyright IBM Corp. <%= YEAR %>, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const require = (await import("node:module")).createRequire(import.meta.url);
 const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
 const __dirname = (await import("node:path")).dirname(__filename);`
   },

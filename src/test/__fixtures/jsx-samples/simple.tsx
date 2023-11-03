85a6f7c08cefbbf99297fcef9c46a961bd2d9856
Joseph D. Harvey <jdharvey@us.ibm.com>
2023-11-03 17:14:28 -0400

    test: update simple.tsx

--- src/test/__fixtures/jsx-samples/simple.tsx
+++ src/test/__fixtures/jsx-samples/simple.tsx
@@ -4,8 +4,12 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
+// @ts-nocheck
 import Button from 'somewhere'
 
+
 const sample = <Button something={1 === 5 ? 'boo' : 'baa'} simple="4">Hey</Button>
+
+const undef = undefined
 
 export default sample

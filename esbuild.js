0311d1d350f9a1ca00fd1d566474d33117560ea4
Francine Lucca <francinelucca@hotmail.com>
2023-12-11 16:55:42 -0500

    fix: moving things around

--- esbuild.js
+++ esbuild.js
@@ -7,9 +7,7 @@
 import { build } from 'esbuild'
 import path from 'path'
 
-await build({
-  banner: {
-    js: `#!/usr/bin/env node
+const banner = `#!/usr/bin/env node
 /*
  * Copyright IBM Corp. 2023, 2023
  *
@@ -19,11 +17,29 @@ await build({
 const require = (await import("node:module")).createRequire(import.meta.url);
 const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
 const __dirname = (await import("node:path")).dirname(__filename);`
+
+await build({
+  banner: {
+    js: banner
   },
   bundle: true,
-  entryPoints: [path.join('dist', 'main', 'index.js'), path.join('dist', 'main', 'collect.js')],
+  entryPoints: [path.join('dist', 'main', 'collect.js')],
   format: 'esm',
-  outdir: 'dist',
+  minify: true,
+  outfile: path.join('dist', 'collect.js'),
+  platform: 'node',
+  target: 'es2021'
+})
+
+await build({
+  banner: {
+    js: banner
+  },
+  bundle: true,
+  entryPoints: [path.join('dist', 'main', 'background-process.js')],
+  format: 'esm',
+  minify: true,
+  outfile: path.join('dist', 'background-process.js'),
   platform: 'node',
   target: 'es2021'
 })
---
+++ src/main/background-process.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright IBM Corp. 2023, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import configSchemaJson from '@ibm/telemetry-config-schema/config.schema.json'
+import { Command } from 'commander'
+
+import { Environment } from './core/environment.js'
+import { createLogFilePath } from './core/log/create-log-file-path.js'
+import { Logger } from './core/log/logger.js'
+import { IbmTelemetry } from './ibm-telemetry.js'
+
+interface CommandLineOptions {
+  config: string
+  log?: string
+}
+
+/**
+ * Sets up Commander, registers the command action, and invokes the action.
+ */
+function run() {
+  const program = new Command()
+    .description('Collect telemetry data for a package.')
+    .requiredOption('--config <config-path>', 'Path to a telemetry configuration file')
+    .option('--log <log-path>', 'Path to temp log file')
+    .action(collect)
+
+  program.parseAsync().catch((err) => {
+    // As a failsafe, this catches any uncaught exception, prints it to stderr, and silently exits
+    console.error(err)
+  })
+}
+
+/**
+ * This is the main entrypoint for telemetry collection.
+ *
+ * @param opts - The command line options provided when the program was executed.
+ */
+async function collect(opts: CommandLineOptions) {
+  const date = new Date().toISOString()
+  const logFilePath = opts.log ?? createLogFilePath(date)
+  const logger = new Logger(logFilePath)
+  const environment = new Environment()
+
+  const ibmTelemetry = new IbmTelemetry(opts.config, configSchemaJson, environment, logger)
+
+  try {
+    await ibmTelemetry.run()
+  } catch (err) {
+    // Catch any exception thrown, log it, and quietly exit
+    if (err instanceof Error) {
+      logger.error(err)
+    } else {
+      logger.error(String(err))
+    }
+  }
+
+  await logger.close()
+}
+
+run()

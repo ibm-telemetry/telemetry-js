e6a627bd1faedd282650f819e93904e7118a9f23
Francine Lucca <francinelucca@hotmail.com>
2023-12-15 20:56:59 -0500

    test: test files

---
+++ src/test/__fixtures/projects/complex-nesting-thingy/mid-level/nested-level-2/test.jsx
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import {Nested2AnotherPackageComp} from 'another-package'
+
+const anotherPackageSample = <Nested2AnotherPackageComp bla bleh="bluh" />
+
+export {
+  anotherPackageSample
+}

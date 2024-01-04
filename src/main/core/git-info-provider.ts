4ef95b3e745d0b00a8e237d9211982bfd4d85428
Joseph D. Harvey <jdharvey@us.ibm.com>
2024-01-04 16:50:08 -0500

    fix: refactor some git-related commands
    Also rework the test and coverage excludes

---
+++ src/main/core/git-info-provider.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright IBM Corp. 2024, 2023
+ *
+ * This source code is licensed under the Apache-2.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { Logger } from './log/logger.js'
+import { Trace } from './log/trace.js'
+import { runCommand } from './run-command.js'
+import { tokenizeRepository } from './tokenize-repository.js'
+
+/**
+ * Instantiable class capable of collecting info about a surrounding git environment.
+ */
+export class GitInfoProvider {
+  private readonly cwd: string
+  private readonly logger: Logger
+
+  /**
+   * Constructs a new telemetry collector.
+   *
+   * @param cwd - Current working directory in which to run the git commands.
+   * @param logger - A logger instance.
+   */
+  public constructor(cwd: string, logger: Logger) {
+    this.cwd = cwd
+    this.logger = logger
+  }
+
+  /**
+   * Gets git-related information for the provided working directory.
+   *
+   * @returns An object containing git info.
+   */
+  @Trace()
+  public async getGitInfo() {
+    // TODO: handle non-existent remote
+    const gitOrigin = (await runCommand('git remote get-url origin', this.logger)).stdout
+    const commitHash = (await runCommand('git rev-parse HEAD', this.logger)).stdout
+    const commitBranches = await this.getCommitBranches(commitHash, this.cwd, this.logger)
+    const commitTags = await this.getCommitTags(commitHash, this.cwd, this.logger)
+    const repository = tokenizeRepository(gitOrigin)
+
+    return { gitOrigin, commitHash, commitBranches, commitTags, repository }
+  }
+
+  /**
+   * Finds and returns the list of branches that point to the HEAD commit.
+   *
+   * @param commitHash - The hash of the commit to obtain branches for.
+   * @param cwd - Current working directory to use as the basis for finding the branches. This
+   * is an absolute path.
+   * @param logger - Logger instance.
+   * @throws An exception if no usable branch data was obtained.
+   * @returns The list of branches that point to the head commit, as an array.
+   */
+  private async getCommitBranches(
+    commitHash: string,
+    cwd: string,
+    logger: Logger
+  ): Promise<string[]> {
+    const allBranches = (
+      await runCommand(
+        `git branch --points-at=${commitHash} --format='%(refname:short)'`,
+        logger,
+        { cwd },
+        true
+      )
+    ).stdout
+      .split(/\r?\n/g)
+      .filter((file) => file !== '')
+
+    return allBranches
+  }
+
+  /**
+   * Finds and returns the list of branches that point to the HEAD commit.
+   *
+   * @param commitHash - The hash of the commit to obtain tags for.
+   * @param cwd - Current working directory to use as the basis for finding the branches. This
+   * is an absolute path.
+   * @param logger - Logger instance.
+   * @throws An exception if no usable branch data was obtained.
+   * @returns The list of branches that point to the head commit, as an array.
+   */
+  private async getCommitTags(commitHash: string, cwd: string, logger: Logger): Promise<string[]> {
+    const allTags = (
+      await runCommand(
+        `git tag --points-at=${commitHash} --format='%(refname:short)'`,
+        logger,
+        { cwd },
+        true
+      )
+    ).stdout
+      .split(/\r?\n/g)
+      .filter((file) => file !== '')
+
+    return allTags
+  }
+}

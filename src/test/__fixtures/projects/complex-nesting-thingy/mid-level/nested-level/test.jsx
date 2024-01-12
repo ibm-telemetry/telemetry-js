1bbaa5c9c4a4a538860da3d7f27d754b889e4a25
Joseph D. Harvey <jdharvey@us.ibm.com>
2024-01-12 11:09:10 -0500

    test: remove some unneeded test fixture files

--- src/test/__fixtures/projects/complex-nesting-thingy/mid-level/nested-level/test.jsx
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import {NestedAnotherPackageComp} from 'another-package'
-
-const anotherPackageSample = <NestedAnotherPackageComp bla bleh="bluh" />
-
-export {
-  anotherPackageSample
-}

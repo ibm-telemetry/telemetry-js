1bea02d2ec09a067e80ac6196754abf3293738f5
Joseph D. Harvey <jdharvey@us.ibm.com>
2024-01-24 16:07:43 -0500

    fix: update copyrights to 2024

--- esbuild.js
+++ esbuild.js
@@ -1,5 +1,5 @@
 /*
- * Copyright IBM Corp. 2023, 2023
+ * Copyright IBM Corp. 2023, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
@@ -9,7 +9,7 @@ import path from 'path'
 
 const banner = `#!/usr/bin/env node
 /*
- * Copyright IBM Corp. 2023, 2023
+ * Copyright IBM Corp. 2023, 2024
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.

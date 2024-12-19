07abbcf830caca852423521bbbe6702c9f7bab69
Ignacio Becerra <i1becerr@ucsd.edu>
2024-12-19 12:07:12 -0800

    feat: adding logs to ChooChooTrain

--- src/main/core/choo-choo-train.ts
+++ src/main/core/choo-choo-train.ts
@@ -6,16 +6,31 @@
  */
 import * as net from 'node:net'
 
+import { CustomResourceAttributes } from '@ibm/telemetry-attributes-js'
 import configSchemaJson from '@ibm/telemetry-config-schema/config.schema.json' assert { type: 'json' }
 
 import { IbmTelemetry } from '../ibm-telemetry.js'
+import { hash } from './anonymize/hash.js'
+import { ConfigValidator } from './config-validator.js'
 import { Environment } from './environment.js'
+import { GitInfoProvider } from './git-info-provider.js'
 import { Loggable } from './log/loggable.js'
 import type { Logger } from './log/logger.js'
 import { Trace } from './log/trace.js'
+import { parseYamlFile } from './parse-yaml-file.js'
 
 const MAX_RETRIES = 3
 const MAX_BACKLOG = 64
+
+interface LogPayload {
+  message: string
+  error?:
+    | {
+        message?: string
+        stack?: string | undefined
+      }
+    | string
+}
 
 interface Work {
   cwd: string
@@ -28,6 +43,10 @@ interface Work {
 export class ChooChooTrain extends Loggable {
   private readonly workQueue: Work[] = []
   private readonly ipcAddr: string
+  private analyzedCommit?: string
+  private analyzedPath?: string
+  private analyzedProjectId?: string
+  private logEndpoint?: string
 
   /**
    * Constructs a new ChooChooTrain instance.
@@ -98,11 +117,20 @@ export class ChooChooTrain extends Loggable {
       server.on('listening', () => {
         resolve(server)
       })
-      server.on('error', reject)
+
+      server.on('error', (error) => {
+        this.sendLogs(
+          `Conductor experienced error on project ${this.analyzedProjectId} against
+          analyzed path ${this.analyzedPath} at commit ${this.analyzedCommit}`,
+          error
+        )
+          .then(() => reject(error))
+          .catch(() => reject(error)) // in case sending logs fails, we still reject promise
+      })
 
       // Set up signal handler to gracefully close the IPC socket
-      process.on('SIGINT', server.close)
-      process.on('SIGTERM', server.close)
+      process.on('SIGINT', () => this.handleSignal(server))
+      process.on('SIGTERM', () => this.handleSignal(server))
 
       server.listen(this.ipcAddr, MAX_BACKLOG)
     })
@@ -114,7 +142,15 @@ export class ChooChooTrain extends Loggable {
       const socket = net.connect(this.ipcAddr)
 
       socket.on('connect', () => resolve(socket))
-      socket.on('error', reject)
+      socket.on('error', (error) => {
+        this.sendLogs(
+          `Wagon experienced error on project ${this.analyzedProjectId} against
+          analyzed path ${this.analyzedPath} at commit ${this.analyzedCommit}`,
+          error
+        )
+          .then(() => reject(error))
+          .catch(() => reject(error)) // in case sending logs fails, we still reject promise
+      })
     })
   }
 
@@ -147,7 +183,16 @@ export class ChooChooTrain extends Loggable {
       this.logger.debug('Sending work through IPC: ', JSON.stringify(work))
 
       socket.on('close', resolve)
-      socket.on('error', reject)
+      socket.on('error', (error) => {
+        this.sendLogs(
+          `Wagon experienced error sending work to conductor 
+          on project ${this.analyzedProjectId} against analyzed 
+          path ${this.analyzedPath} at commit ${this.analyzedCommit}`,
+          error
+        )
+          .then(() => reject(error))
+          .catch(() => reject(error)) // in case sending logs fails, we still reject promise
+      })
       socket.on('timeout', reject)
 
       socket.write(Buffer.from(JSON.stringify(work)))
@@ -161,6 +206,21 @@ export class ChooChooTrain extends Loggable {
       'We are the conductor of the choo-choo train. Running all available work in queue'
     )
 
+    const start = performance.now()
+
+    // Both server and clients will have the same data due to being provided from git,
+    // thus we obtain the data from the conductor's first job before the loop
+    const conductorWork = this.workQueue?.[0]
+    if (conductorWork) {
+      await this.getRepoData(conductorWork)
+
+      this.sendLogs(
+        `The ChooChooTrain ride started for analyzed path ${this.analyzedPath} at commit ${this.analyzedCommit}`
+      )
+    }
+
+    let totalWork = 0
+
     // Consume work until the queue is empty
     while (this.workQueue.length > 0) {
       this.logger.debug('Queue length', this.workQueue.length)
@@ -173,7 +233,15 @@ export class ChooChooTrain extends Loggable {
 
       // collect for current work
       await this.collect(new Environment({ cwd: currentWork.cwd }), currentWork.configFilePath)
+      totalWork++
     }
+
+    const totalTime = (performance.now() - start).toFixed(2)
+
+    this.sendLogs(
+      `The ChooChooTrain Ride with ${totalWork} packages at analyzed path ${this.analyzedPath} 
+      at commit ${this.analyzedCommit} took ${totalTime}ms`
+    )
 
     server.close()
   }
@@ -199,8 +267,96 @@ export class ChooChooTrain extends Loggable {
       // Catch any exception thrown, log it, and quietly exit
       if (err instanceof Error) {
         this.logger.error(err)
+        this.sendLogs('Process signal error: ', err)
       } else {
         this.logger.error(String(err))
+        this.sendLogs('Process signal error: ', String(err))
+      }
+    }
+  }
+
+  @Trace()
+  private async getRepoData(work: Work) {
+    const { repository, commitHash } = await new GitInfoProvider(work.cwd, this.logger).getGitInfo()
+
+    const hashedData = hash(
+      {
+        [CustomResourceAttributes.ANALYZED_COMMIT]: commitHash,
+        [CustomResourceAttributes.ANALYZED_PATH]: `${repository.host ?? ''}/${
+          repository.owner ?? ''
+        }/${repository.repository ?? ''}`
+      },
+      [CustomResourceAttributes.ANALYZED_COMMIT, CustomResourceAttributes.ANALYZED_PATH]
+    )
+
+    this.analyzedCommit = hashedData[CustomResourceAttributes.ANALYZED_COMMIT]
+    this.analyzedPath = hashedData[CustomResourceAttributes.ANALYZED_PATH]
+    const config = await parseYamlFile(work.configFilePath)
+    const configValidator: ConfigValidator = new ConfigValidator(configSchemaJson, this.logger)
+    configValidator.validate(config)
+
+    this.analyzedProjectId = config.projectId
+    this.logEndpoint = config.endpoint as string
+  }
+
+  @Trace()
+  private handleSignal(server: net.Server) {
+    server.close((err) => {
+      if (err) {
+        this.sendLogs('Process signal error: ', err).catch((sendErr) => {
+          if (sendErr instanceof Error) {
+            this.logger.error(sendErr)
+          } else {
+            this.logger.error(String(sendErr))
+          }
+        })
+      }
+    })
+  }
+
+  /**
+   * This function handles sending logs to the collector.
+   * There are two types of logs this function can send out:
+   *   1. ChooChooTrain start and end logs
+   *   2. Error logs.
+   *
+   * @param message - The message to send to collector.
+   * @param error - The optional error that caused the train to crash.
+   */
+  @Trace()
+  private async sendLogs(message: string, error?: Error | string) {
+    if (this.logEndpoint === '') return
+
+    const payload: LogPayload = {
+      message: message
+    }
+
+    if (error != undefined) {
+      if (error instanceof Error) {
+        payload.error = {
+          message: error.message,
+          stack: error.stack
+        }
+      } else {
+        payload.error = error
+      }
+    }
+
+    try {
+      const response = await fetch(this.logEndpoint, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+      })
+
+      if (!response.ok) {
+        this.logger.error(`Failed to send log: ${response.statusText}`)
+      }
+    } catch (sendErr) {
+      if (sendErr instanceof Error) {
+        this.logger.error(sendErr)
+      } else {
+        this.logger.error(String(sendErr))
       }
     }
   }

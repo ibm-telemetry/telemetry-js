378af6bf01b8e8d52903fa54098c8ab0d3f0d48a
Ignacio Becerra <i1becerr@ucsd.edu>
2025-01-07 15:13:11 -0800

    fix: specify the name is also hashed

--- src/main/core/environment.ts
+++ src/main/core/environment.ts
@@ -31,7 +31,7 @@ export class Environment {
   readonly isCI: boolean
 
   /**
-   * The name of the possible continguous integration environment the process is running in.
+   * The hashed name of the possible continguous integration environment the process is running in.
    */
   name: string
 

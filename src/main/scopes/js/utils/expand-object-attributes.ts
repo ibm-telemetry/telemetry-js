63f92316e6c3b4a403a125093a50f3eb7f7e0acf
Warren Blood <blood.warren@gmail.com>
2026-09-25 14:28:27 -0400

    docs: headers

--- src/main/scopes/js/utils/expand-object-attributes.ts
+++ src/main/scopes/js/utils/expand-object-attributes.ts
@@ -4,13 +4,6 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-/*
- * Copyright IBM Corp. 2026, 2026
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import { ComplexValue } from '../complex-value.js'
 
 /**
--- src/test/scopes/js/node-handlers/value-handlers/object-literal-expression-handler.test.ts
+++ src/test/scopes/js/node-handlers/value-handlers/object-literal-expression-handler.test.ts
@@ -4,12 +4,6 @@
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
-/*
- * Copyright IBM Corp. 2026, 2026
- *
- * This source code is licensed under the Apache-2.0 license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import * as ts from 'typescript'
 import { describe, expect, it } from 'vitest'
 
